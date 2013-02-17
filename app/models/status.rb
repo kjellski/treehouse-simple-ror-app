68ef4a0c4438e035d18a9ef2ff469aeeaa870a29
kjellski <otto.kjell@gmail.com>
2013-02-17 19:02:44 +0100

    refactored the routes to login and register for the ease of use. ouch, refactored the model to require a user for each status, forgot that earlier...

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,5 +1,7 @@
 class Status < ActiveRecord::Base
   attr_accessible :content, :user_id
 
+  validates :user, presence: true
+
   belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Treehouse::Application.routes.draw do
   devise_for :users
 
+  devise_scope :user do
+    get 'register',
+        to: 'devise/registrations#new',
+        as: :register
+
+    get 'login',
+        to: 'devise/sessions#new',
+        as: :login
+  end
+
   resources :statuses
+  root :to => 'statuses#index'
 
 
   # The priority is based upon order of creation:
@@ -53,7 +64,6 @@ Treehouse::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'statuses#index'
 
   # See how all your routes lay out with "rake routes"
 
