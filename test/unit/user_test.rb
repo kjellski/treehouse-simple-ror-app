059cbe354d3741a527ecb2100f596f0deb65cfac
kjellski <otto.kjell@gmail.com>
2013-03-16 16:38:53 +0100

    added a better test for profile names in order to reflect the names that are really allowed refactored the regex for profile names to match everything in the given name

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -28,10 +28,21 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "a user should have a profile name without spaces" do
-    user = User.new
+    user = User.new(first_name: 'kjellski', last_name: 'otto', email: 'otto.kjell@aslaksd.com')
+    user.password = user.password_confirmation = 'asdfasdf'
+
     user.profile_name = "This is a wrong profile name"
+
     assert !user.save
     assert !user.errors[:profile_name].empty?
     assert user.errors[:profile_name].include?("Must be formatted correctly.")
+  end
+
+  test "a user can have a correctly formatted profile name" do
+    user = User.new(first_name: 'kjellski', last_name: 'otto', email: 'otto.kjell@aslaksd.com')
+    user.password = user.password_confirmation = 'asdfasdf'
+    user.profile_name = 'kjellski1'
+
+    assert user.valid?
   end
 end
