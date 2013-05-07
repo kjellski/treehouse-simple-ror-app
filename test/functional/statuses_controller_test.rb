8838369b31a99351d0f41982e31199c7f83aa49f
kjellski <otto.kjell@gmail.com>
2013-05-07 10:28:58 +0200

    Scoped status updates and added tests for allowing posting only from the current user itself.

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -29,7 +29,7 @@ class StatusesControllerTest < ActionController::TestCase
     assert_redirected_to new_user_session_path
   end
 
-  test "should create status" do
+  test "should create status when logged in" do
     sign_in users(:kjellski)
 
     assert_difference('Status.count') do
@@ -37,6 +37,17 @@ class StatusesControllerTest < ActionController::TestCase
     end
 
     assert_redirected_to status_path(assigns(:status))
+  end
+
+  test "should create status for the current user when logged in" do
+    sign_in users(:kjellski)
+
+    assert_difference('Status.count') do
+      post :create, status: { user_id: users(:anna).id, content: @status.content }
+    end
+
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:kjellski).id
   end
 
   test "should show status" do
@@ -56,10 +67,26 @@ class StatusesControllerTest < ActionController::TestCase
     assert_redirected_to new_user_session_path
   end
 
-  test "should update status" do
+  test "should update status when logged in" do
     sign_in users(:kjellski)
     put :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
+  end
+
+  test "should update status for the current user when logged in" do
+    sign_in users(:kjellski)
+
+    put :update, id: @status, status: { content: @status.content, user_id: users(:anna).id }
+
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:kjellski).id
+  end
+
+  test "should not update the status if no if nothing has changed" do
+    sign_in users(:kjellski)
+    put :update, id: @status
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:kjellski).id
   end
 
   test "should be logged in to update a status" do
